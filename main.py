d797f9dad05e19e5242e7048e02734f3a6d4caa5
cintsa <120549056+cintsa@users.noreply.github.com>
2022-12-16 14:51:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -29,20 +29,23 @@ class CamApp(App):
         # define a video capture object
         vid = cv2.VideoCapture(0)
 
-        while(True):
+        if(vid.isOpened()==False):
+         print("Error opening video")
+
+
+        while(vid.isOpened()):
       
            # Capture the video frame
            # by frame
            ret, frame = vid.read()
-  
-           # Display the resulting frame
-           cv2.imshow('frame', frame)
-      
-           # the 'q' button is set as the
-           # quitting button you may use any
-           # desired button of your choice
-           if cv2.waitKey(1) & 0xFF == ord('q'):
+           if(ret==True):  
+             # Display the resulting frame
+             cv2.imshow('frame', frame)  
+ 
+             if cv2.waitKey(25) & 0xFF == ord('q'):
                break
+           else:
+             break
   
         # After the loop release the cap object
         vid.release()   
